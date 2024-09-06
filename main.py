c164f6f3246bb0e410710ff53b3c2a8f6c0c91a4
Vadim Black <vvchernyh85@gmail.com>
2024-09-06 22:40:03 +0300

    added a few feature

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from handlers.other import other_router
 from handlers.user import user_router
@@ -31,7 +33,7 @@ logger = logging.getLogger(__name__)
 async def main():
     config = load_config()
 
-    bot = Bot(token=config.tg_bot.token)
+    bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
     dp.include_router(user_router)
